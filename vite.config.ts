b760fb83ef0ebeaa9a5826d1a31cf2488708395c
Easha Fernandes <easha.ferns12@gmail.com>
2026-01-11 16:55:45 +0530

    NAme schanges

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // Set this to your repository name
-  base: '/portfolio/',
+  base: '/Easha-Fernandes-Portfolio/',
   server: {
     port: 3000,
   },
